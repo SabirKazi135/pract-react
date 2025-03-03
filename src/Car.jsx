0f74f8dc83d214b656b4c1c65bbbc2802206ff79
SabirKazi135 <sabirkazi135@gmail.com>
2025-03-03 12:34:53 +0530

    commit

---
+++ src/Car.jsx
@@ -0,0 +1,3 @@
+function Car(prop) {
+  return <h2>hi, I am a {prop.color} Car!</h2>;
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
+
+
+function Garage() {
+  return (
+    <>
+      <h1>Who Lives in my Garage</h1>
+      <Car color="red" />
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Garage />);
