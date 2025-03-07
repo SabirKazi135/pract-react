716164c9fb898a2e67c9ef1c83154647ac660e9b
SabirKazi135 <sabirkazi135@gmail.com>
2025-03-07 15:34:29 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import React from "react";
 import ReactDom from "react-dom/client";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<>Hello World Sabir</>);
+root.render(
+  <>
+    <ul>
+      <li>item1</li>
+      <li>item2</li>
+      <li>item3</li>
+    </ul>
+  </>
+);
