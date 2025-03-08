72b0dcad23671eaf80b11ff1ee11bcf73ae9ed11
SabirKazi135 <sabirkazi135@gmail.com>
2025-03-08 15:33:33 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(
-  <>
-    <ul>
-      <li>item1</li>
-      <li>item2</li>
-      <li>item3</li>
-    </ul>
-  </>
-);
+root.render(<h1 className="header">hello</h1>);
