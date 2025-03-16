0111fd71c8ce06537944806735f86631cce01bd5
SabirKazi135 <sabirkazi135@gmail.com>
2025-03-16 16:14:32 +0530

    commit

--- src/List.jsx
+++
@@ -1,9 +0,0 @@
-function List() {
-  const fruits = ["aplle", "orange", "coconut", "pineapple"];
-  const ListItems = fruits.map((fruit) => {
-    return <li>{fruit}</li>;
-  });
-  return <ol>{ListItems}</ol>;
-}
-
-export default List;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import App from "./App.jsx";
 import "./App.css";
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <>
+    <h1>Hello, Sabir</h1>
+  </>
+);
