9b9ac152cb628113bca9020e3d49fd14d06f56a5
SabirKazi135 <sabirkazi135@gmail.com>
2025-03-18 16:11:10 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import "./App.css";
+
+class Car {
+  constructor(name) {
+    this.brand = name;
+  }
+
+  present() {
+    return "I have a " + this.brand;
+  }
+}
+
+const mycar = new Car("Ford");
+
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <h1>Hello, Sabir</h1>
+    <h1>this is the car {mycar.brand}</h1>
+    <p>hello, {mycar.present()}</p>
   </>
 );
