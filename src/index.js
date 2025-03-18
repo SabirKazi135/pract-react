0e596a6ad49f05b9b6bd8c7d32e61ab675977fcb
SabirKazi135 <sabirkazi135@gmail.com>
2025-03-18 16:34:04 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,36 +2,13 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import "./App.css";
 
-class Car {
-  constructor(name) {
-    this.brand = name;
-  }
-
-  present() {
-    return "I have a " + this.brand;
-  }
+function hello() {
+  return "Hello World Sabir";
 }
-
-class Model extends Car {
-  constructor(name, mod) {
-    super(name);
-    this.model = mod;
-  }
-
-  show() {
-    return this.present() + "', it is a " + this.model;
-  }
-}
-
-const mycar = new Car("Ford");
-const myfor = new Model("Toyota", "Fortune");
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <h1>this is the car {mycar.brand}</h1>
-    <p>hello, {mycar.present()}</p>
-    <h1>this is the car {myfor.brand}</h1>
-    <p>hello, {myfor.show()}</p>
+    <h1>{hello()}</h1>
   </>
 );
