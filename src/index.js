b2c9d28a8be1d3dbaa16299c5e80a5b9759bd3d9
SabirKazi135 <sabirkazi135@gmail.com>
2025-03-18 16:57:21 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import "./App.css";
 
-function hello() {
-  return "Hello World Sabir";
+const x = 5;
+let text = "GoodBye";
+if (x < 10) {
+  text = "Hello";
 }
-
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(
-  <>
-    <h1>{hello()}</h1>
-  </>
-);
+root.render(text);
