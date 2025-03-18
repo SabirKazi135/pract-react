14ee49cdd959ff7c221534c6c343bd614cc97b0a
SabirKazi135 <sabirkazi135@gmail.com>
2025-03-18 16:18:12 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -12,12 +12,26 @@ class Car {
   }
 }
 
+class Model extends Car {
+  constructor(name, mod) {
+    super(name);
+    this.model = mod;
+  }
+
+  show() {
+    return this.present() + "', it is a " + this.model;
+  }
+}
+
 const mycar = new Car("Ford");
+const myfor = new Model("Toyota", "Fortune");
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <>
     <h1>this is the car {mycar.brand}</h1>
     <p>hello, {mycar.present()}</p>
+    <h1>this is the car {myfor.brand}</h1>
+    <p>hello, {myfor.show()}</p>
   </>
 );
