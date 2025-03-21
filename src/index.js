117d14542ee009359a768a3ce20dc0980ab752cf
SabirKazi135 <sabirkazi135@gmail.com>
2025-03-21 20:21:59 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import "./App.css";
 
-function getGreeting(name) {
-  return `Hello,  ${name}`;
-}
+const array = ["Apple", "Banana", "Orange"];
+const arr = array.map((ar) => {
+  return <li>{ar}</li>;
+});
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <h1>{getGreeting("Sabir")}</h1>
+    <ul>{arr}</ul>
   </>
 );
