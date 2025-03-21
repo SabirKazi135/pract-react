0bcb8ae3746b29d40488183288b7415302127f2b
SabirKazi135 <sabirkazi135@gmail.com>
2025-03-21 21:10:06 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./App.css";
-
-function Car(props) {
-  return <h2>Hi, I am a {props.color} Car!</h2>;
-}
+import Car from "./Car";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Car color="red" />);
