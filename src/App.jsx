4e8e2cf192913601afcbe8ae1451aeb691beafb6
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-05 20:09:46 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import ShowName from "./ShowName";
 function App() {
   return (
     <>
-      <ShowName name="Sabir" />
-      <ShowName name="Taif" />
-      <ShowName name="Raees" />
-      <ShowName name="Siddiq" />
-      <ShowName name="Kazi" />
+      <ShowName name="Sabir" age={20} />
+      <ShowName name="Taif" age={21} />
+      <ShowName name="Raees" age={40} />
+      <ShowName name="Siddiq" age={50} />
+      <ShowName name="Kazi" age={60} />
     </>
   );
 }
--- src/ShowName.jsx
+++ src/ShowName.jsx
@@ -1,5 +1,10 @@
 function ShowName(props) {
-  return <h3>Hi, I am {props.name}, Nice to meet You</h3>;
+  return (
+    <>
+      <h3>Hi, I am {props.name}, Nice to meet You.</h3>
+      <p>And my age is: {props.age}</p>
+    </>
+  );
 }
 
 export default ShowName;
