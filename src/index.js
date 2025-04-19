4be9921465436a10f30a8c306fd15ac032ddaf2b
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-19 17:14:51 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom/client";
 import { useState } from "react";
 import "./App.css";
 
-function App() {
+function Button() {
+  const [textState, setState] = useState("hidden");
+
   return (
     
-  );
+  )
+}
+
+function App() {
+  return <Button />;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
