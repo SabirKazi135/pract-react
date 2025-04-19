8ab728d34d2f4c5f9d83f09ead9c696205e24ef3
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-19 16:07:55 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,23 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./App.css";
 
-function Button() {
-  const handleClick = (e) => {
-    e.target.textContent = "ouch";
-  };
-  return (
-    <>
-      <button onClick={(e) => handleClick(e)}>Click Me</button>
-    </>
-  );
-}
 
 function App() {
-  return (
-    <>
-      <Button />
-    </>
-  );
+  return ();
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
