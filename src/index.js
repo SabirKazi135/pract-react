ffa1d78731d84909b41bcbd06c1ad6735b9c7240
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-19 16:42:17 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,36 +3,9 @@ import ReactDOM from "react-dom/client";
 import { useState } from "react";
 import "./App.css";
 
-function Clock() {
-  const p = document.getElementById("p");
-  const [count, setCount] = useState(0);
-  function decreament() {
-    setCount(count - 1);
-  }
-  function increament() {
-    setCount(count + 1);
-  }
-  function reset() {
-    setCount(0);
-  }
-  return (
-    <>
-      <div>
-        <h1>Counter</h1>
-        <p id="p">{count}</p>
-        <button onClick={decreament}>-</button>
-        <button onClick={reset}>Reset</button>
-        <button onClick={increament}>+</button>
-      </div>
-    </>
-  );
-}
-
 function App() {
   return (
-    <>
-      <Clock />
-    </>
+    
   );
 }
 
