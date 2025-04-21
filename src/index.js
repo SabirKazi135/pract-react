577eb27949b04f55e58b6a710e9909f8bc67f245
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-21 20:00:27 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [car, setCar] = useState({year: 2024,
-    make: "ford",
-    model: "Mustag"}
-  );
+  const [car, setCar] = useState({ year: 2024, make: "ford", model: "Mustag" });
   return (
+    <>
+      <p>
+        Your fav car is: {car.year} {car.make} {car.model}
+      </p>
+      <input type="number" value={car.year} />
+      <br />
+      <input type="text" value={car.make} />
+      <br />
+      <input type="text" value={car.model} />
+      <br />
+    </>
   );
 }
 
