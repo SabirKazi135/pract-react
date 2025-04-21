fdc9ade32af7e4c7e3d35ba824e94b2162f18e77
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-21 16:47:18 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,31 +4,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function increment() {
-    setCount((c) => c + 1);
-    setCount((c) => c + 1);
-    setCount((c) => c + 1);
-  }
-
-  function decrement() {
-    setCount((c) => c - 1);
-    setCount((c) => c - 1);
-    setCount((c) => c - 1);
-  }
-
-  function reset() {
-    setCount(0);
-  }
-
   return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={reset}>Reset</button>
-      <button onClick={increment}>Increment</button>
-    </div>
   );
 }
 
