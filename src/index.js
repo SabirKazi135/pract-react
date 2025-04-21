89a7a8f00ce68eb0a228ae285c06b7845e814799
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-21 16:11:15 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,53 +4,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("Guest");
-  const [quantity, setQuantity] = useState(10);
-  const [comment, setComment] = useState("");
-  const [payment, setPayment] = useState("");
-
-  function handleQuantityChange(event) {
-    setQuantity(event.target.value);
-  }
-  function handleChange(event) {
-    setName(event.target.value);
-  }
-  function handleCommentChange(event) {
-    setComment(event.target.value);
-  }
-
-  function handlePaymentChange(event) {
-    setPayment(event.target.value);
-  }
   return (
-    <>
-      <input type="text" value={name} onChange={handleChange} />
-      <p>Name: {name}</p>
-      <input type="number" value={quantity} onChange={handleQuantityChange} />
-      <p>Number: {quantity}</p>
-      <textarea
-        name=""
-        id=""
-        value={comment}
-        onChange={handleCommentChange}
-        placeholder="Enter Your Review"
-      ></textarea>
-      <p>comment: {comment}</p>
-      <select name="" id="" value={payment} onChange={handlePaymentChange}>
-        <option value="">Select an option</option>
-        <option value="rupay">rupay</option>
-        <option value="mastercard">mastercard</option>
-        <option value="rupe">visa</option>
-      </select>
-      <p
-        style={{
-          display: "inline-block",
-          marginLeft: "10px",
-        }}
-      >
-        Payment: {payment}
-      </p>
-    </>
   );
 }
 
