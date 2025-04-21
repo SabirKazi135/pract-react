df343d251ef2cfd1a54a77946b264d0cd1d2eea3
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-21 16:34:58 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,27 +3,32 @@ import ReactDOM from "react-dom/client";
 import { useState } from "react";
 import "./App.css";
 
-function ColorPicker() {
-  const [color, setColor] = useState("#ffffff");
-  function handleColorChange(event) {
-    setColor(event.target.value);
-  }
-  return (
-    <div className="color-picker-container">
-      <h1>Color Picker</h1>
-      <div className="color-display" style={{ background: color }}>
-        <p>Selected Color: {color}</p>
-      </div>
-      <label htmlFor="">Select a color:</label>
-      <input type="color" value={color} onChange={handleColorChange} />
-    </div>
-  );
-}
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount((c) => c + 1);
+    setCount((c) => c + 1);
+    setCount((c) => c + 1);
+  }
+
+  function decrement() {
+    setCount((c) => c - 1);
+    setCount((c) => c - 1);
+    setCount((c) => c - 1);
+  }
+
+  function reset() {
+    setCount(0);
+  }
+
   return (
-    <>
-      <ColorPicker />
-    </>
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={increment}>Increment</button>
+    </div>
   );
 }
 
