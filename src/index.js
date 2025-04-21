c91b074b8bf7b809382cc7131d29e90c57b17053
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-21 16:18:31 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import "./App.css";
 
 function ColorPicker() {
   const [color, setColor] = useState("#ffffff");
+  function handleColorChange(event) {
+    setColor(event.target.value);
+  }
   return (
-    <>
-    </>
+    <div className="color-picker-container">
+      <h1>Color Picker</h1>
+      <div className="color-display" style={{ background: color }}>
+        <p>Selected Color: {color}</p>
+      </div>
+      <label htmlFor="">Select a color:</label>
+      <input type="color" value={color} onChange={handleColorChange} />
+    </div>
   );
 }
 function App() {
