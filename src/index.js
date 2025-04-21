6c6d246f1e4d9bc07f9cac80ed7575f6e55c74bb
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-21 08:48:01 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { useState } from "react";
-import "./App.css";
-
-function Button() {
-  const [textBack, setBack] = useState("red");
-  function change() {
-    if (textBack === "red") {
-      setBack("green");
-    } else {
-      setBack("red");
-    }
-  }
-  return (
-    <>
-      <p style={{ background: textBack }}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut perspiciatis
-        nulla eligendi, quae, reiciendis veniam quod quam, labore placeat
-        voluptas commodi consequuntur sit distinctio eius enim laborum nisi
-        nihil dolore.
-      </p>
-      <button onClick={change}>change</button>
-    </>
-  );
-}
 
 function App() {
-  return <Button />;
+  return (
+    <form action="">
+      <label htmlFor="">
+        Enter Your Name:
+        <input type="text" />
+      </label>
+    </form>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
