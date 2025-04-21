a271fb13a59ca852fb285cfb4980edf98f8abe90
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-21 20:02:38 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import "./App.css";
 
 function App() {
   const [car, setCar] = useState({ year: 2024, make: "ford", model: "Mustag" });
+  function handleYearChange(e) {}
+  function handleMakeChange(e) {}
+  function handleModelChange(e) {}
   return (
     <>
       <p>
         Your fav car is: {car.year} {car.make} {car.model}
       </p>
-      <input type="number" value={car.year} />
+      <input type="number" value={car.year} onChange={handleYearChange} />
       <br />
-      <input type="text" value={car.make} />
+      <input type="text" value={car.make} onChange={handleMakeChange} />
       <br />
-      <input type="text" value={car.model} />
+      <input type="text" value={car.model} onChange={handleModelChange} />
       <br />
     </>
   );
