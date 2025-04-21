3bf5e7f7e77c608fb5758271d19ac19e0aea97ed
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-21 16:00:50 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "./App.css";
 function App() {
   const [name, setName] = useState("Guest");
   const [quantity, setQuantity] = useState(10);
+  const [comment, setComment] = useState("");
 
   function handleQuantityChange(event) {
     setQuantity(event.target.value);
@@ -13,12 +14,22 @@ function App() {
   function handleChange(event) {
     setName(event.target.value);
   }
+  function handleCommentChange(event) {
+    setComment(event.target.value);
+  }
   return (
     <>
       <input type="text" value={name} onChange={handleChange} />
       <p>Name: {name}</p>
       <input type="number" value={quantity} onChange={handleQuantityChange} />
       <p>Number: {quantity}</p>
+      <textarea
+        name=""
+        id=""
+        value={comment}
+        onChange={handleChange}
+      ></textarea>
+      <p>comment: {comment}</p>
     </>
   );
 }
