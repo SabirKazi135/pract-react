65ffb3a400b32dfdfb9a08c1bf68ea646fdce7e6
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-21 20:18:44 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,16 +4,21 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [foods ,setFoods ] = useState(["Apple", "Orange", "Banana"]);
+  const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
 
-  function addFood(e) {
-    
-  }
+  function addFood(e) {}
 
-  function removeFood(e) {
-    
-  }
+  function removeFood(e) {}
   return (
+    <>
+      <h2>List of Goods</h2>
+      <ul>
+        {foods.map((food, index) => {
+          return <li key={index}>{food}</li>;
+        })}
+      </ul>
+      <input type="text" id="foodinput" placeholder="Enter food name" />
+    </>
   );
 }
 
