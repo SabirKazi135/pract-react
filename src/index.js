1d8bd2c1c985bd81aa01d7034269613139965a21
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-22 14:31:03 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,55 +4,17 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [fruits, setFruits] = useState(["Apple", "Orange", "banana"]);
-  function addFruit() {
-    const newFruit = document.getElementById("newFruit").value;
-    setFruits((f) => [...f, newFruit]);
-    document.getElementById("newFruit").value = "";
-  }
+  const [cars, setCars] = useState([]);
+  const [carYear, setCarYear] = useState(new Date().getFullYear());
+  const [carMake, setCarMake] = useState("");
+  const [carModel, setCarModel] = useState("");
 
-  function removeFruit(i) {
-    setFruits((pf) =>
-      pf.filter((_, index) => {
-        return i !== index;
-      })
-    );
-  }
+  function addCar() {}
 
-  function editFruit(i) {
-    const newName = prompt("New name of fruit");
-    setFruits((f) => {
-      const copy = [...f];
-      copy[i] = newName;
-      return copy;
-    });
-  }
-  return (
-    <>
-      <h1>List of Fruits</h1>
-      <ul>
-        {fruits.map((fruit, index) => {
-          return (
-            <>
-              <li
-                style={{
-                  display: "inline-block",
-                }}
-                key={index}
-                onClick={() => removeFruit(index)}
-              >
-                {fruit}
-              </li>
-              <button onClick={() => editFruit(index)}>edit</button>
-              <br />
-            </>
-          );
-        })}
-      </ul>
-      <input type="text" id="newFruit" />
-      <button onClick={addFruit}>Add</button>
-    </>
-  );
+  function removeCar() {}
+
+  function yearChange() {}
+  return <></>;
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
