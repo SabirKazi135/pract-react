e328018a292d843acf6d6a045e7edd38a99bd077
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-22 08:40:51 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  
+  const [inputText, setInputText] = useState("");
+  function changeInput(e) {
+    setInputText(e.target.value);
+  }
+  return (
+    <>
+      <input type="text" value={inputText} onChange={changeInput} />
+      <p>Text: {inputText}</p>
+    </>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
