f46a6c36c430aeb6cb01dc45645d76e919fa6c5a
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-22 08:44:27 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import "./App.css";
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [inputNumber, setInputNumber] = useState("");
   function changeInput(e) {
     setInputText(e.target.value);
+  }
+  function changeNumber(e) {
+    setInputNumber(e.target.value);
   }
   return (
     <>
       <input type="text" value={inputText} onChange={changeInput} />
       <p>Text: {inputText}</p>
+      <input type="number" value={inputNumber} onChange={changeNumber} />
+      <p>Number: {inputNumber}</p>
     </>
   );
 }
