e1471367548d7cbcb1ea7cdf7455c89349797810
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-22 09:30:32 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,16 +4,32 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  function increament() {
-    setCount((c) => c + 1);
-    setCount((c) => c + 1);
-    setCount((c) => c + 1);
+  const [profile, setProfile] = useState({
+    name: "Guest",
+    location: {
+      city: "Unknown",
+    },
+  });
+
+  function change(e) {
+    const name = e.target.name;
+    const value = e.target.value;
   }
   return (
     <>
-      <p>{count}</p>
-      <button onClick={increament}>increament</button>
+      <h2>
+        UserName: {profile.name}, <br />
+        CityNames:
+        <ul>{profile.location.city}</ul>
+      </h2>
+      <label>
+        Name:
+        <input type="text" name="name" onChange={change} />
+      </label>
+      <label>
+        Location:
+        <input type="text" name="location" onChange={change} />
+      </label>
     </>
   );
 }
