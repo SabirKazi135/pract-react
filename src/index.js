4c009f086bcaf935213082df6cfbb04c7ea95468
SabirKazi135 <sabirkazi135@gmail.com>
2025-04-22 08:33:27 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,32 +4,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
-  function addFood(e) {
-    const newFood = document.getElementById("foodinput").value;
-    document.getElementById("foodinput").value = "";
-    setFoods((f) => [...f, newFood]);
-  }
-
-  function removeFood(index) {
-    setFoods(foods.filter((_, i) => i !== index));
-  }
-  return (
-    <>
-      <h2>List of Goods</h2>
-      <ul>
-        {foods.map((food, index) => {
-          return (
-            <li key={index} onClick={() => removeFood(index)}>
-              {food}
-            </li>
-          );
-        })}
-      </ul>
-      <input type="text" id="foodinput" placeholder="Enter food name" />
-      <button onClick={addFood}>Add Food</button>
-    </>
-  );
+  
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
