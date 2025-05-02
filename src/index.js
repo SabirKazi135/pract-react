37ea24131e1e3acdc95a354f6934f61661294fe7
SabirKazi135 <sabirkazi135@gmail.com>
2025-05-02 23:56:18 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  function addCount() {
+    setCount((c) => c + 1);
+  }
   return (
     <>
       <p>Count: {count}</p>
+      <button onClick={addCount}>Add</button>
     </>
   );
 }
