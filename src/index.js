81d3b25e2dd2c9d48f391e682b1729ec87ec16cb
SabirKazi135 <sabirkazi135@gmail.com>
2025-05-02 23:55:13 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <>
-      < />
+      <p>Count: {count}</p>
     </>
   );
 }
