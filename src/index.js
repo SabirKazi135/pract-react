c91289f9fdc4f2f29660d3c86e1dcab214b3ba71
SabirKazi135 <sabirkazi135@gmail.com>
2025-05-03 00:02:38 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Count ${count}`;
+  }, [count]);
+
   function addCount() {
     setCount((c) => c + 1);
+  }
+  function subtractCount() {
+    setCount((c) => c - 1);
   }
   return (
     <>
       <p>Count: {count}</p>
       <button onClick={addCount}>Add</button>
+      <button onClick={subtractCount}>Subtract</button>
     </>
   );
 }
