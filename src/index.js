a5a87f3b0c0731a4562a5f8ec4160e0745821c5a
sabir <sabir@root.root>
2025-05-11 15:37:38 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,36 +2,5 @@ import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState("green");
-
-  useEffect(() => {
-    document.title = `Count ${count} ${color}`;
-    return () => {
-      
-    };
-  }, [count]);
-
-  function addCount() {
-    setCount((c) => c + 1);
-  }
-  function subtractCount() {
-    setCount((c) => c - 1);
-  }
-  function changeColor() {
-    setColor((c) => (c === "green" ? "red" : "green"));
-  }
-  return (
-    <>
-      <p style={{ color: color }}>Count: {count}</p>
-      <button onClick={addCount}>Add</button>
-      <button onClick={subtractCount}>Subtract</button>
-      <br />
-      <button onClick={changeColor}>Change Color</button>
-    </>
-  );
-}
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
