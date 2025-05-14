5f5516de62e485c34e36aecbe738b9442ca47777
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-14 18:25:51 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { useState } from "react";
-function name(params) {}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
