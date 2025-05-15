b19b3ad5a0c9c6b1f75be62885a078ab05e2e0c4
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-15 22:46:21 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-function App() {
-  const [name, setName] = useState("");
-  const [character, setCharacter] = useState(0);
-
-  useEffect(() => {
-    setCharacter(name.length);
-  }, [name]);
-
-  function changeName(e) {
-    setName(e.target.value);
-  }
-  return (
-    <>
-      <div>Name: {name}</div>
-      <input type="text" value={name} onChange={(e) => changeName(e)} />
-      <p>Total Character: {character}</p>
-    </>
-  );
-}
 
 root.render(<App />);
