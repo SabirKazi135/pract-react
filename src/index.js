ce0f2a6815f255eeef66350860275ccb23c2977e
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-15 16:33:04 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,42 +3,10 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function App() {
-  const [show, setShow] = useState(true);
-  return (
-    <>
-      <button onClick={() => setShow((s) => !s)}>Toggle Component</button>
-      {show && <ResizeTracker />}
-    </>
-  );
-}
-
-function ResizeTracker() {
-  const [width, setWidth] = useState(window.innerWidth);
-  const [height, setHeight] = useState(window.innerHeight);
-
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    console.log("Event Listener Added");
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-      console.log("Event Listener Removed");
-    };
-  }, []);
-
-  useEffect(() => {
-    document.title = `Size: ${width} x ${height}`;
-  }, [width, height]);
-
-  function handleResize() {
-    setWidth(window.innerWidth);
-    setHeight(window.innerHeight);
-  }
 
   return (
     <>
-      <p>Window Width: {width}px</p>
-      <p>Window Height: {height}px</p>
+      
     </>
   );
 }
