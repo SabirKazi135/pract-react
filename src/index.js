61c673a71708055a6b5cf2714fccf3f299cd9527
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-15 15:44:41 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -5,16 +5,22 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function App() {
   const [count, setCount] = useState(0);
-  useEffect(() => {
-    document.title = `Count ${count}`;
-  });
+  const [n, setN] = useState(0);
+
   function addCount() {
     setCount((c) => c + 1);
+  }
+  const [nor, setNor] = useState(0);
+  function normal() {
+    setNor((n) => n + 1);
   }
   return (
     <>
       <p>Count: {count}</p>
       <button onClick={addCount}>Add</button>
+      <p>------------------------</p>
+      <p>{nor}</p>
+      <button onClick={normal}>normal</button>
     </>
   );
 }
