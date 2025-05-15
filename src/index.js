076d80b54108f6345dde1683a7a318c068ca730f
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-15 22:04:50 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 function App() {
   const [count, setCount] = useState(0);
   useEffect(() => {}, []);
+  function addCount() {
+    setCount((c) => c + 1);
+  }
   return (
     <>
       <div>Count: {count}</div>
