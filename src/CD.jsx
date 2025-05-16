9d9dc58d2e0c3bf086660889561b88f143b7d937
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-16 14:56:43 +0530

    commit

--- src/CD.jsx
+++ src/CD.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
+import { UserContext } from "./CA.jsx";
 function CD() {
+  const user = useContext(UserContext);
   return (
     <div className="box">
       <h1>Component D</h1>
+      <h4>bye: {user}</h4>
     </div>
   );
 }
