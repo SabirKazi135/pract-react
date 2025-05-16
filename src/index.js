caf87d347b019d3fdf6892c8cd375116b6d1e8b9
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-16 23:44:34 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { react, useState, useRef } from "react";
+
+function App() {
+  const [number, setNumber] = useState(0);
+
+  return (
+    <div>
+      <button>Click Me</button>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<App />);
