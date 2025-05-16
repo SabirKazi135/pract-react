9436368170234662bdf8b09c4782b44fa29e1fac
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-16 14:46:38 +0530

    commit

--- src/CA.jsx
+++ src/CA.jsx
@@ -1,10 +1,13 @@
 import CB from "./CB";
+import react, { useState } from "react";
 
 function CA() {
+  const [user, setUser] = useState("SabirKazi");
   return (
     <div className="box">
-          <h1>Component A</h1>
-          <CB />
+      <h1>Component A</h1>
+      <h3>Hello: {user}</h3>
+      <CB />
     </div>
   );
 }
