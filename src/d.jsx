c17f80cfc1aca3ed42dcb79a200cfeb8b7ed7338
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-17 14:38:29 +0530

    commit

--- src/d.jsx
+++ src/d.jsx
@@ -1,5 +1,14 @@
+import { useContext } from "react";
+import { UserContext } from "./index.js";
+
 function D() {
-    return ();
+  const userText = useContext(UserContext);
+  return (
+    <>
+      <h2>htis is amother</h2>
+      <p>passed msg is : {userText}</p>
+    </>
+  );
 }
 
 export default D;
