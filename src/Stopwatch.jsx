4258108d447b5c80249e5736d49fc6597e0de24c
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-18 21:28:38 +0530

    commit

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -1,15 +1,18 @@
 import react, { useRef, useEffect } from "react";
 
 function StopWatch() {
-    const [hour, setHours] = useState(0);
-    const [minutes, setMinutes] = useState(0);
-    const [, ] = useState(0);
+  const [hour, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
   return (
     <div className="main">
       <h1>Stopwatch</h1>
       <div>
         {hour}:{minutes}:{seconds}
       </div>
+      <button className="start">Start</button>
+      <button className="stop">Stop</button>
+      <button className="reset">Reset</button>
     </div>
   );
 }
