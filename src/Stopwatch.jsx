5d45a5650715d9506bdf9001f8d1fe9290bddd72
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-18 21:21:51 +0530

    commit

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -1,9 +1,17 @@
+import { react, useRef, useEffect } from "react";
+
 function StopWatch() {
-    return (
-        <div className="main">
-            
-        </div>
-    );
+  const hour = useRef(0);
+  const minutes = useRef(0);
+  const seconds = useRef(0);
+  return (
+    <div className="main">
+      <h1>Stopwatch</h1>
+      <div>
+        {hour}:{minutes}:{seconds}
+      </div>
+    </div>
+  );
 }
 
 export default StopWatch;
