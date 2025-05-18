3d75a66568eddd3fb7ced517f1b838aacd4866f5
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-18 23:35:59 +0530

    commit

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -1,62 +1,11 @@
 import react, { useRef, useEffect, useState } from "react";
 
 function StopWatch() {
-  const [hour, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
-
-  const startTime = useRef(null);
-
-  useEffect(() => {
-    if (seconds >= 100) {
-      setSeconds(0);
-      setMinutes((m) => m + 1);
-    }
-  }, [seconds]);
-
-  useEffect(() => {
-    if (minutes >= 60) {
-      setMinutes(0);
-      setHours((m) => m + 1);
-    }
-  }, [minutes]);
-
-  function start() {
-    if (startTime.current !== null) {
-      return;
-    }
-    startTime.current = setInterval(() => {
-      setSeconds((c) => c + 1);
-    }, 10);
-  }
-
-  function stop() {
-    clearInterval(startTime.current);
-    startTime.current = null;
-  }
-
-  function reset() {
-    stop();
-    setHours(0);
-    setMinutes(0);
-    setSeconds(0);
-  }
+  const [score, setScore] = useState(0);
   return (
-    <div className="main">
-      <h1>Stopwatch</h1>
-      <div>
-        {String(hour).padStart(2, "0")}:{String(minutes).padStart(2, "0")}:
-        {String(seconds).padStart(2, "0")}
-      </div>
-      <button className="start" onClick={start}>
-        Start
-      </button>
-      <button className="stop" onClick={stop}>
-        Stop
-      </button>
-      <button className="reset" onClick={reset}>
-        Reset
-      </button>
+    <div className="main1">
+      <h2 className="head">Counter</h2>
+      <p className="counter">{score}</p>
     </div>
   );
 }
